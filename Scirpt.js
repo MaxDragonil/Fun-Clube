f65fac0e1b7e40c0b913c13374275a1ac98729dc
MaxDragonil <kidiyadeep@gmail.com>
2021-12-02 06:16:24 +0530

    Owned it! Done
    Mostly done.

---
+++ Scirpt.js
@@ -0,0 +1,12 @@
+console.log('hey there!')
+
+
+function ShowPopup () {
+    var Popup = document.getElementById('popup')
+    Popup.classList.add('show')
+}
+
+function hidePopup () {
+    var Popup = document.getElementById('popup')
+    Popup.classList.add('show')
+}
